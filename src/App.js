81b674bc84463fb8a0b1b5fa7d76e4550ded351b
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-26 19:59:10 +0400

    Adding the states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './app.css';
 
 
 function App() {
+  const [selectedPolitican, setSelectedPolitician] = useState("");
+  const [politicianData, setPoliticianData] = useState({});
+
+  useEffect(() => {
+    fetch("./data.json")
+    .then(response => response.json())
+    .then(value => {
+      console.log(value);
+    })
+  }, []);
+
+  const selectHandler = e => setSelectedPolitician(e.target.value);
+
   return (
     <div>
-      <p>Hello World!</p>
+      <label>აირჩიე პოლიტიკოსი</label>
+      <br />
+      <select onChange={selectHandler}>
+        <option>ვახტანგ მეგრელიშვილი</option>
+        <option>იაგო ხვიჩია</option>
+        <option>ალექსანდრე რაქვიაშვილი</option>
+        <option>ჰერმან საბო</option>
+        <option>ოთარ ზაკალაშვილი</option>
+      </select>
     </div>
   );
 }
