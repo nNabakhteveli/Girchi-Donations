fcceff9cdc546f83c17488c71fc0144c1bd4d5be
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-26 23:25:38 +0400

    Added table component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,80 @@
 import React, { useState, useEffect } from 'react';
-import './app.css';
+import axios from 'axios';
+import './App.css';
+
+const DataTable = props => {
+  return(
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.donation}</td>
+    </tr>
+  );
+}
+
+const TableTemplate = ({ name, donation, key }) => {
+  return(
+    <div key={key}>
+      <table>
+        <tbody>
+          <tr>
+            <th>მომხმარებელი</th>
+            <th>შემოწირულობა</th>
+          </tr>
+          <DataTable name={name} donation={donation} />
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 
 function App() {
   const [selectedPolitican, setSelectedPolitician] = useState("");
   const [politicianData, setPoliticianData] = useState({});
+  const [fetchedData, setFetchedData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    fetch("./data.json")
-    .then(response => response.json())
-    .then(value => {
-      console.log(value);
-    })
+    axios.get("./data.json").then(response => {
+      setFetchedData(response.data.politicians)
+    });
   }, []);
 
-  const selectHandler = e => setSelectedPolitician(e.target.value);
+  // useEffect(() => {
+  //   console.log(politicianData.donations[0]);
+  // }, [selectedPolitican])
+  
+  let randomKey = Math.random() * 500000000;
+  
+  const selectHandler = e => {
+    setLoaded(false);
+    setSelectedPolitician(e.target.value);
+    setPoliticianData(fetchedData.find(politician => politician.name === e.target.value)); 
+    setLoaded(true);
+  }
+  
+  // if(loaded) {
+    // console.log(politicianData.donations);
+    // [politicianData].map(value => <DataTable name={value.donations.donator} donation={value.amount} />);
+  // }
+
+
 
   return (
     <div>
+      <h1>{politicianData.name}</h1>
       <label>აირჩიე პოლიტიკოსი</label>
       <br />
       <select onChange={selectHandler}>
-        <option>ვახტანგ მეგრელიშვილი</option>
-        <option>იაგო ხვიჩია</option>
-        <option>ალექსანდრე რაქვიაშვილი</option>
-        <option>ჰერმან საბო</option>
-        <option>ოთარ ზაკალაშვილი</option>
+        <option value="Vakhtang_Megrelishvili">ვახტანგ მეგრელიშვილი</option>
+        <option value="Iago_Khvichia">იაგო ხვიჩია</option>
+        <option value="Aleksandre_Rakviashvili">ალექსანდრე რაქვიაშვილი</option>
+        <option value="Herman_Szaboo">ჰერმან საბო</option>
+        <option value="Otar_Zakalashvili">ოთარ ზაკალაშვილი</option>
       </select>
+
+      {loaded ? politicianData.donations.map(value => <TableTemplate name={value.donator} donation={value.amount} key={randomKey} />) : null }
+      
     </div>
   );
 }
