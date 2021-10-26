0585e206ba0407bf672231b1a85bfec2614b8841
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-27 01:37:59 +0400

    fixed filter

--- src/App.js
+++ src/App.js
@@ -2,31 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const DataTable = props => {
+
+const DonationsTable = ({ name, donation }) => {
   return(
-    <tr>
-      <td>{props.name}</td>
-      <td>{props.donation}</td>
+    <tr key={Math.random() * 10000000000000}>
+      <td>{name}</td>
+      <td>{donation}</td>
     </tr>
   );
 }
-
-const TableTemplate = ({ name, donation, key }) => {
-  return(
-    <div key={key}>
-      <table>
-        <tbody>
-          <tr>
-            <th>მომხმარებელი</th>
-            <th>შემოწირულობა</th>
-          </tr>
-          <DataTable name={name} donation={donation} />
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
 
 function App() {
   const [selectedPolitican, setSelectedPolitician] = useState("");
@@ -39,12 +23,6 @@ function App() {
       setFetchedData(response.data.politicians)
     });
   }, []);
-
-  // useEffect(() => {
-  //   console.log(politicianData.donations[0]);
-  // }, [selectedPolitican])
-  
-  let randomKey = Math.random() * 500000000;
   
   const selectHandler = e => {
     setLoaded(false);
@@ -53,12 +31,6 @@ function App() {
     setLoaded(true);
   }
   
-  // if(loaded) {
-    // console.log(politicianData.donations);
-    // [politicianData].map(value => <DataTable name={value.donations.donator} donation={value.amount} />);
-  // }
-
-
 
   return (
     <div>
@@ -73,8 +45,16 @@ function App() {
         <option value="Otar_Zakalashvili">ოთარ ზაკალაშვილი</option>
       </select>
 
-      {loaded ? politicianData.donations.map(value => <TableTemplate name={value.donator} donation={value.amount} key={randomKey} />) : null }
-      
+
+      <table className="table">
+        <tbody key={Math.random() * 10000000000000}>
+          <tr>
+            <th>მომხმარებელი</th>
+            <th>შემოწირულობა</th>
+          </tr>
+          {loaded ? politicianData.donations.map(value => <DonationsTable name={value.donator} donation={value.amount} />) : null }
+        </tbody>
+      </table>
     </div>
   );
 }
