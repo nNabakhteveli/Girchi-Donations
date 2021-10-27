dbe2498d7c0ee788c34b8edfc9eecf0ab7b95169
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-28 00:03:22 +0400

    Fixed date filtering

--- src/App.js
+++ src/App.js
@@ -5,32 +5,31 @@ import './App.css';
 
 const FilterByDate = props => {
   return(
-    <select onChange={props.filterHandler} className="filter-by-date">
-      <option defaultValue disabled selected>აირჩიე თვე</option>
-      <option value="October">ოქტომბერი</option>
-      <option value="September">სექტემბერი</option>
+    <select key={10} onChange={props.filterHandler} className="filter-by-date" defaultValue={"აირჩიე თვე"}>
+      <option defaultValue disabled key={11}>აირჩიე თვე</option>
+      <option value="October" key={12}>ოქტომბერი</option>
+      <option value="September" key={13}>სექტემბერი</option>
     </select>
   );
 }
 
 const DonationsTable = ({ name, donation, date }) => {
   return(
-    <tr key={Math.random() * 100000000} className="userProps">
-      <td key={Math.random() * 100000000 + 3.14}>{name}</td>
-      <td key={Math.random() * 100000000}>{donation}</td>
-      <td key={Math.random() * 100000000}>{date}</td>
+    <tr key={6} className="userProps">
+      <td key={7}>{name}</td>
+      <td key={8}>{donation} ₾</td>
+      <td key={9}>{date}</td>
     </tr>
   );
 }
 
 function App() {
-  const [selectedPolitican, setSelectedPolitican] = useState("");
   const [politicanData, setPoliticanData] = useState({});
   const [filteredDonations, setFilteredDonations] = useState([]);
   const [fetchedData, setFetchedData] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [isLoaded, setLoaded] = useState(false);
+  const [startDate, setStartDate] = useState({});
+  const [endDate, setEndDate] = useState({});
 
 
   useEffect(() => {
@@ -43,80 +42,69 @@ function App() {
   const selectHandler = e => {
     setFilteredDonations([]);
     setLoaded(false);
-    setSelectedPolitican(e.target.value);
     setPoliticanData(fetchedData.find(politican => politican.name === e.target.value)); 
     setLoaded(true);
   }
 
-  
+
+  const iterableData = filteredDonations.length > 0 ? filteredDonations : politicanData.donations;
+  // let startingDate = new Date(startDate), endingDate = new Date(endDate);
   const filterHandler = e => {
     switch(e.target.value) {
       case "October": 
-        setStartDate("2021-10-01");
-        setEndDate("2021-10-31");
+        setStartDate(new Date("2021-10-01"));
+        setEndDate(new Date("2021-10-31"));
         break;
-  
+        
       case "September":
-        setStartDate("2021-09-01");
-        setEndDate("2021-09-30");
+        setStartDate(new Date("2021-09-01"));
+        setEndDate(new Date("2021-09-30"));
         break;
   
       default: 
-        console.log("hi");
+      console.log("hi");
     }
-    dateFilter(politicanData.donations);
   }
 
-  const dateFilter = (donationsArr) => {
-    const instanceObj = {...politicanData};
-    let startingDate = new Date(startDate), endingDate = new Date(endDate);   
-    let arr = [];
-
-    donationsArr.forEach(value => {
-      let date = new Date(value.date);
-      if(date >= startingDate && date <= endingDate) {
-        arr.push(value);
-      } 
-    })
-    setFilteredDonations(arr);
-  }
-
-  let iterableData = filteredDonations.length > 0 ? filteredDonations : politicanData.donations;
-  
-  // useEffect(() => {
-  //   let instance = {...politicanData};
-  //   instance.donations = filteredDonations;
-
-  //   setPoliticanData(instance);
-    
-  // }, [filteredDonations])
-
+  useEffect(() => {
+    if(isLoaded) {
+      const donationsArr = politicanData.donations;
+      let arr = [];
+      
+      for(const value of donationsArr) {
+        let date = new Date(value.date);
+        if(date >= startDate && date <= endDate) {
+          arr.push(value);
+        } 
+      }
+      setFilteredDonations(arr);
+    }
+  }, [startDate])
 
   
   return (
     <div>
-      <h1>{politicanData.name}</h1>
       <label>აირჩიე პოლიტიკოსი</label>
       <br />
-      <select onChange={selectHandler}>
-        <option value="Vakhtang_Megrelishvili">ვახტანგ მეგრელიშვილი</option>
-        <option value="Iago_Khvichia">იაგო ხვიჩია</option>
-        <option value="Aleksandre_Rakviashvili">ალექსანდრე რაქვიაშვილი</option>
-        <option value="Herman_Szaboo">ჰერმან საბო</option>
-        <option value="Otar_Zakalashvili">ოთარ ზაკალაშვილი</option>
+      <select onChange={selectHandler} key={20}>
+        <option value="Vakhtang_Megrelishvili" key={14}>ვახტანგ მეგრელიშვილი</option>
+        <option value="Iago_Khvichia" key={15}>იაგო ხვიჩია</option>
+        <option value="Aleksandre_Rakviashvili" key={16}>ალექსანდრე რაქვიაშვილი</option>
+        <option value="Herman_Szaboo" key={17}>ჰერმან საბო</option>
+        <option value="Otar_Zakalashvili" key={18}>ოთარ ზაკალაშვილი</option>
       </select>
 
-      { loaded ? <FilterByDate filterHandler={filterHandler} /> : null }
-
-      <table className="table" key={Math.random() * 100000000}>
-        <tbody key={13}>
-          <tr key={1} className="table-headers">
-            <th key={2}>მომხმარებელი</th>             
-            <th key={3}>შემოწირულობა</th>
+      { isLoaded ? <FilterByDate filterHandler={filterHandler} /> : null }
+      <p className="politican-name">პოლიტიკოსი:{politicanData.name_ge}</p>
+      <table className="table" key={21}>
+        <tbody key={1}>
+          <tr key={2} className="table-headers">
+            <th key={3}>მომხმარებელი</th>             
+            <th key={4}>შემოწირულობა</th>
             <th key={5}>თარიღი</th>
           </tr>
           {
-            loaded ? iterableData.map(value => <DonationsTable name={value.donator} donation={value.amount} date={value.date} />) : null 
+            isLoaded ? iterableData.map(value => <DonationsTable name={value.donator} donation={value.amount} date={value.date} />) : null 
           }
         </tbody>
       </table>
