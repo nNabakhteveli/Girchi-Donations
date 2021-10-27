8ea437b0eb79a39e139f7febbe78632f81976bf8
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-28 03:21:55 +0400

    Added url parsing and deleted unused packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Url from 'url-parse';
 import './App.css';
 
 
-const FilterByDate = props => {
+const DynamicTableRows = props => {
+  if(!props.hasQueryString) {
+    return(
+      props.isLoaded ? props.iterableData.map(
+        value => <DonationsTable name={value.donator} donation={value.amount} date={value.date} />) : null 
+    );
+  }
+  return props.iterableData.map(value => <DonationsTable name={value.donator} donation={value.amount} date={value.date} />)
+}
+
+const Table = props => {
   return(
-    <select key={10} onChange={props.filterHandler} className="filter-by-date" defaultValue={"აირჩიე თვე"}>
+  <table className="table" key={21}>
+    <tbody key="table-body">
+      <tr key="static-row" className="table-headers">
+        <th key="user">მომხმარებელი</th>             
+        <th key="user-donation">შემოწირულობა</th>
+        <th key="donation-date">თარიღი</th>
+      </tr>
+      <DynamicTableRows hasQueryString={props.hasQueryString} isLoaded={props.isLoaded} iterableData={props.iterableData} />
+    </tbody>
+  </table>
+  );
+}
+
+const FilterByDate = ({ filterHandler }) => {
+  return(
+    <select key={10} onChange={filterHandler} className="filter-by-date" defaultValue={"აირჩიე თვე"}>
       <option defaultValue disabled key={11}>აირჩიე თვე</option>
       <option value="October" key={12}>ოქტომბერი</option>
       <option value="September" key={13}>სექტემბერი</option>
@@ -15,10 +41,10 @@ const FilterByDate = props => {
 
 const DonationsTable = ({ name, donation, date }) => {
   return(
-    <tr key={6} className="userProps">
-      <td key={7}>{name}</td>
-      <td key={8}>{donation} ₾</td>
-      <td key={9}>{date}</td>
+    <tr key="changableList" className="userProps">
+      <td key="name">{name}</td>
+      <td key="donation">{donation} ₾</td>
+      <td key="date">{date}</td>
     </tr>
   );
 }
@@ -27,28 +53,38 @@ function App() {
   const [politicanData, setPoliticanData] = useState({});
   const [filteredDonations, setFilteredDonations] = useState([]);
   const [fetchedData, setFetchedData] = useState([]);
+  const [hasQueryString, setHasQueryString] = useState(false);  
   const [isLoaded, setLoaded] = useState(false);
   const [startDate, setStartDate] = useState({});
   const [endDate, setEndDate] = useState({});
 
-
+  
   useEffect(() => {
+    let parsedUrlQuery = new Url(window.location.href, true);
+
     axios.get("./data.json").then(response => {
       setFetchedData(response.data.politicans)
+
+      if(Object.keys(parsedUrlQuery.query).length > 0) {  
+        let filteredData = response.data.politicans.find(value => value.name === parsedUrlQuery.query.politican)
+        setPoliticanData(filteredData);
+        setHasQueryString(true);
+        setLoaded(true);
+      }
     });
   }, []);
 
 
   const selectHandler = e => {
+    if(hasQueryString) {
+      return;
+    }
     setFilteredDonations([]);
     setLoaded(false);
     setPoliticanData(fetchedData.find(politican => politican.name === e.target.value)); 
     setLoaded(true);
   }
 
-
-  const iterableData = filteredDonations.length > 0 ? filteredDonations : politicanData.donations;
-  // let startingDate = new Date(startDate), endingDate = new Date(endDate);
   const filterHandler = e => {
     switch(e.target.value) {
       case "October": 
@@ -61,8 +97,7 @@ function App() {
         setEndDate(new Date("2021-09-30"));
         break;
   
-      default: 
-      console.log("hi");
+      default: break;
     }
   }
 
@@ -81,33 +116,25 @@ function App() {
     }
   }, [startDate])
 
-  
+  const iterableData = filteredDonations.length > 0 ? filteredDonations : politicanData.donations;
+
   return (
     <div>
-      <label>აირჩიე პოლიტიკოსი</label>
-      <br />
-      <select onChange={selectHandler} key={20}>
-        <option value="Vakhtang_Megrelishvili" key={14}>ვახტანგ მეგრელიშვილი</option>
-        <option value="Iago_Khvichia" key={15}>იაგო ხვიჩია</option>
-        <option value="Aleksandre_Rakviashvili" key={16}>ალექსანდრე რაქვიაშვილი</option>
-        <option value="Herman_Szaboo" key={17}>ჰერმან საბო</option>
-        <option value="Otar_Zakalashvili" key={18}>ოთარ ზაკალაშვილი</option>
-      </select>
-
+        <div className="select-politican">
+        <label>აირჩიე პოლიტიკოსი</label>
+        <br />
+        <select onChange={selectHandler} key={20}>
+          <option value="Vakhtang_Megrelishvili" key={14}>ვახტანგ მეგრელიშვილი</option>
+          <option value="Iago_Khvichia" key={15}>იაგო ხვიჩია</option>
+          <option value="Aleksandre_Rakviashvili" key={16}>ალექსანდრე რაქვიაშვილი</option>
+          <option value="Herman_Szaboo" key={17}>ჰერმან საბო</option>
+          <option value="Otar_Zakalashvili" key={18}>ოთარ ზაკალაშვილი</option>
+        </select>
+      </div>
       { isLoaded ? <FilterByDate filterHandler={filterHandler} /> : null }
-      <p className="politican-name">პოლიტიკოსი:{politicanData.name_ge}</p>
-      <table className="table" key={21}>
-        <tbody key={1}>
-          <tr key={2} className="table-headers">
-            <th key={3}>მომხმარებელი</th>             
-            <th key={4}>შემოწირულობა</th>
-            <th key={5}>თარიღი</th>
-          </tr>
-          {
-            isLoaded ? iterableData.map(value => <DonationsTable name={value.donator} donation={value.amount} date={value.date} />) : null 
-          }
-        </tbody>
-      </table>
+      { isLoaded ? <p className="politican-name">პოლიტიკოსი: {politicanData.name_ge}</p> : null }
+      
+      <Table isLoaded={isLoaded} iterableData={iterableData} hasQueryString={hasQueryString} />
     </div>
   );
 }
